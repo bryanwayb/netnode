1dc940021b7578c7a1f44e0e30f78f21e8afe841
Bryan Way <bryanwayb@gmail.com>
2015-03-06 14:56:08 -0600

    Fixed incorrect data type

--- NetNode/Core.cs
+++ NetNode/Core.cs
@@ -5,7 +5,7 @@ namespace NetNode
 	public struct NodePayload
 	{
 		public uint flags;				// Flags to be received by a node server
-		public byte extraSize;			// Size of extra data.
+		public uint extraSize;			// Size of extra data.
 		public byte[] extra;			// Extra data set for internal use by the Node. Contents are based on the payload flags.
 		public uint signatureSize;		// Size of signature
 		public byte[] signature;		// Signature identifier, used to identify binded function
@@ -17,7 +17,10 @@ namespace NetNode
 	{
 		public static Node Default = new Node();
 
+		private static byte[] PayloadMagic = new byte[4] { (byte)'n', (byte)'o', (byte)'d', (byte)'e' }; // Required to be the very first 4 bytes of every request.
+		
 		private Encoding Encoder = Encoding.UTF8;
+		
 		public void SetEncoder(Encoding encoder)
 		{
 			this.Encoder = encoder;
